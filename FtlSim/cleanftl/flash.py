e963e602e705285832f0977207a23eb5ab492094
Jun He <jhe@cs.wisc.edu>
2015-06-15 01:49:00 -0500

    class recorder is working

--- FtlSim/cleanftl/flash.py
+++ FtlSim/cleanftl/flash.py
@@ -1,18 +1,16 @@
-import recorder
-
 class Flash(object):
     def __init__(self, recorder):
         self.recorder = recorder
 
     def page_read(self, pagenum, cat):
-        print 'page_read', pagenum, cat
-        # recorder.put('page_read', pagenum, cat)
+        # print 'page_read', pagenum, cat
+        self.recorder.put('page_read', pagenum, cat)
 
     def page_write(self, pagenum, cat):
-        print 'page_write', pagenum, cat
-        # recorder.put('page_write', pagenum, cat)
+        # print 'page_write', pagenum, cat
+        self.recorder.put('page_write', pagenum, cat)
 
     def block_erase(self, blocknum, cat):
-        print 'block_erase', blocknum, cat
-        # recorder.put('block_erase', blocknum, cat)
+        # print 'block_erase', blocknum, cat
+        self.recorder.put('block_erase', blocknum, cat)
 
