df12c0d0023be21a9b30dcbb51d10e14a3e7cca0
Jun He <jhe@cs.wisc.edu>
2015-06-23 23:15:08 -0500

    commit by Makefile

--- Makefile.py
+++ Makefile.py
@@ -264,9 +264,9 @@ def pure_sequential_or_random():
         workflow(conf)
 
 def main():
-    shcmd("sudo -u jun git pull && "\
-        "sudo -u jun git commit -am 'commit by Makefile' && "\
-        "sudo -u jun git push")
+    shcmd("sudo -u jun git pull")
+    shcmd("sudo -u jun git commit -am 'commit by Makefile'", ignore_error=True)
+    shcmd("sudo -u jun git push")
     #function you want to call
     # parse_blkparse('./bigsample', 'myresult')
     # shcmd("scp jun@192.168.56.102:/tmp/ftlsim.in ./FtlSim/misc/")
