42e8b80e433b7a2f6c1a4fe97fa21d70921766e5
Jun He <jhe@cs.wisc.edu>
2015-06-26 12:38:31 -0500

    add nonblockingreader

---
+++ WlRunner/nonblockingreader.py
@@ -0,0 +1,38 @@
+import sys, threading, Queue
+
+def enqueue_lines(f, line_queue):
+    for line in iter(f.readline, b''):
+        line_queue.put(line)
+
+class NonBlockingReader(object):
+    def __init__(self, file_path):
+        """Note that the thread is started once the instance is created"""
+        self.file_path = file_path
+        self.f = open(file_path, 'r')
+        self.q = Queue.Queue()
+        self.t = threading.Thread(target=enqueue_lines,
+            args=(self.f, self.q))
+        self.t.daemon = True # thread dies with the program
+        self.t.start()
+
+    def readline(self):
+        """return None is there is nothing"""
+        try:
+            line = self.q.get_nowait()
+        except Queue.Empty:
+            return None
+        else:
+            return line
+
+    def __del__(self):
+        self.f.close()
+
+
+if __name__ == '__main__':
+    nb_reader = NonBlockingReader("/sys/kernel/debug/tracing/trace_pipe")
+
+    while True:
+        line = nb_reader.readline()
+        print line,
+
+
