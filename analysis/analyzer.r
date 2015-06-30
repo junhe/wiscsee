a7e97502b085ec8b12b9a849d3cbd50ff55971a3
Jun He <jhe@cs.wisc.edu>
2015-06-30 16:00:45 -0500

    update analysis/analyzer.r by get_github_url.py

--- analysis/analyzer.r
+++ analysis/analyzer.r
@@ -896,7 +896,8 @@ explore.sim.results <- function()
 
     # local_main("~/datahouse/long-mdtest/")
     # local_main("~/datahouse/ext4-hybridmap-4096/")
-    local_main("~/datahouse/ext4-hybridmap-512/")
+    # local_main("~/datahouse/ext4-hybridmap-512/")
+    local_main("~/datahouse/sqlbench-1by1")
 }
 
 explore.mywl <- function()
