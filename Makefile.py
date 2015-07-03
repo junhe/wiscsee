b1c3e65f34ea52164b51bf2eb25c274083b6b52c
Jun He <jhe@cs.wisc.edu>
2015-07-03 16:51:48 -0500

    commit by Makefile:

--- Makefile.py
+++ Makefile.py
@@ -396,6 +396,7 @@ def synthetic_on_filesystems():
         "loop_dev_size_mb"      : None,
         "tmpfs_mount_point"     : "/mnt/tmpfs",
         "fs_mount_point"        : "/mnt/fsonloop",
+        "common_mnt_opts"       : "-o discard",
 
 
         "sector_size"           : 512,
@@ -407,7 +408,7 @@ def synthetic_on_filesystems():
         "Synthetic" :{
             "chunk_count": 100*2**20/(8*1024),
             "chunk_size" : 8*1024,
-            "iterations" : 2
+            "iterations" : 3
         },
 
         # if you choose LBAGENERATOR for workload_src, the following will
