be1f7cd9a039e48553ea146d3999785a40ced875
Jun He <jhe@cs.wisc.edu>
2015-07-17 15:03:54 -0500

    add dftl.py. currently it only has some notes

---
+++ FtlSim/dftl.py
@@ -0,0 +1,86 @@
+import bitarray
+from collections import deque
+import os
+
+import bidict
+
+import config
+import ftlbuilder
+import recorder
+
+"""
+Notes for DFTL design
+
+Components
+- block pool: it should have free list, used data blocks and used translation
+  blocks. We should be able to find out the next free block from here. The DFTL
+  paper does not mention a in-RAM data structure like this. How do they find
+  out the next free block?
+- Cached Mapping Table (CMT): this class should do the following:
+    - have logical page <-> physical page mapping entries.
+    - be able to translation LPN to PPN and PPN to LPN
+    - implement a replacement policy
+    - has a size() method to output the size of the CMT, so we know it does not
+      exceed the size of SRAM
+    - be able to add one entry
+    - be able to remove one entry
+    - be able to find the proper entry to be moved.
+    - be able to set the number of entries allows by size
+    - be able to set the number of entries directly
+    - be able to fetch a mapping entry (this need to consult the Global
+      Translation Directory)
+    - be able to evict one entrie to flash
+    - be able to evict entries to flash in batch
+
+    - CMT interacts with
+        - Flash: to save and read translation pages
+        - block pool: to find free block to evict translation pages
+        - Global Translation Directory: to find out where the translation pages
+          are (so you can read/write), also, you need to update GTD when evicting
+          pages to flash.
+        - bitmap???
+
+- Global Translation Directory, this should do the following:
+    - maintains the locations of translation pages
+    - given a Virtual Translation Page Number, find out the physical
+      translation page number
+    - given a Logical Data Page Number, find out the physical data page number
+
+    - GTD should be a pretty passive class, it interacts with
+        - CMT. When CMT changes the location of translation pages, GTD should
+          be updated to reflect the changes
+
+- Garbage Collector
+    - clean data blocks: to not interrupt current writing of pages, garbage
+      collector should have its own appending point to write the garbage
+      collected data
+    - clean translation blocks. This cleaning should also have its own
+      appending point.
+    - NOTE: the DFTL paper says DFTL also have partial merge and switch merge,
+      I need to read their code to find out why.
+
+    - How the garbage collector should interact with other components?
+        - this cleaner will use free blocks and make used block free, so it
+          will need to interact with block pool to move blocks between
+          different lists.
+        - the cleaner also need to interact with CMT because it may move
+          translation pages around
+        - the cleaner also need to interact with bitmap because it needs to
+          find out the if a page is valid or not.  It also need to find out the
+          invalid ratio of the blocks
+        - the cleaner also need to update the global translation directory
+          since it moved pages.
+        - NOTE: all the classes above should a provide an easy interface for
+          the cleaner to use, so the cleaner does not need to use the low-level
+          interfaces to implement these functions
+
+- Appending points: there should be several appending points:
+    - appending point for writing translation page
+    - appending point for writing data page
+    - appending ponit for garbage collection
+    - NOTE: these points should be maintained by block pool.
+"""
+
+
+
+
