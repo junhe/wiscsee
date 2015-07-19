549c4905b44f7dc0c067c4891931db669a87a742
Jun He <jhe@cs.wisc.edu>
2015-07-19 15:25:03 -0500

    checkpoint

--- FtlSim/dftl.py
+++ FtlSim/dftl.py
@@ -16,8 +16,8 @@ Components
   blocks. We should be able to find out the next free block from here. The DFTL
   paper does not mention a in-RAM data structure like this. How do they find
   out the next free block?
-    - it manages the appending point for different purpose. No, it does not have
-    enough info to do that.
+    - it manages the appending point for different purpose. No, it does not
+      have enough info to do that.
 
 - Cached Mapping Table (CMT): this class should do the following:
     - have logical page <-> physical page mapping entries.
@@ -39,20 +39,25 @@ Components
         - Flash: to save and read translation pages
         - block pool: to find free block to evict translation pages
         - Global Translation Directory: to find out where the translation pages
-          are (so you can read/write), also, you need to update GTD when evicting
-          pages to flash.
+          are (so you can read/write), also, you need to update GTD when
+          evicting pages to flash.
         - bitmap???
 
-- Global mapping table (GMT): this class holds the mappings of all the data pages. In
-  real implementation, this should be stored in flash. This data structure will be used
-  intensively. It should have a good interface.
+- Global mapping table (GMT): this class holds the mappings of all the data
+  pages. In real implementation, this should be stored in flash. This data
+  structure will be used intensively. It should have a good interface.
     - API, get_entries_of_Mvpn(virtual mapping page number). This is one of the
-      cases that it may be used: when reading a data page, its translation entry is not
-      in CMT. The translator will consult the GTD to find the physical
-      translation page number of virtual translation page number V. Then we need to get the
-      content of V by reading its corresponding physical page. We may provide an interface
-      to the translator like load_translation_physical_page(PPN), which internally, we
-      read from GMT.
+      cases that it may be used: when reading a data page, its translation
+      entry is not in CMT. The translator will consult the GTD to find the
+      physical translation page number of virtual translation page number V.
+      Then we need to get the content of V by reading its corresponding
+      physical page. We may provide an interface to the translator like
+      load_translation_physical_page(PPN), which internally, we read from GMT.
+
+- Out of Band Area (OOB) of a page: it can hold:
+    - page state (Invalid, Valid, Erased)
+    - logical page number
+    - ECC
 
 - Global Translation Directory, this should do the following:
     - maintains the locations of translation pages
@@ -143,10 +148,31 @@ class BlockPool(object):
             ' '.join('trans_usedblocks', repr(self.trans_usedblocks)) + '\n' \
             ' '.join('data_usedblocks', repr(self.data_usedblocks)) + '\n'
 
+class CachedMappingTable(object):
+    """
+    """
+    def __init__(self, confobj):
+        # let's begin by using simple dict, more advanced structure needed later
+        # it holds lpn->ppn
+        self.entries = {}
+
+    def lpn_to_ppn(self, lpn):
+        "Try to find ppn of the given lpn in cache"
+        return self.entries.get(lpn, None)
+
+    def add_entry(self, lpn, ppn):
+        if self.entries.has_key(lpn):
+            raise RuntimeError("{}:{} already exists in CMT entries.".format(
+                lpn, self.entries[lpn]))
+        self.entries[lpn] = ppn
+
+    def remove_entry_by_lpn(self, lpn):
+        del self.entries[lpn]
 
 class GlobalMappingTable(object):
     """
     This mapping table is for data pages, not for translation pages.
+    GMT should have entries as many as the number of pages in flash
     """
     def __init__(self, confobj, flashobj):
         """
@@ -158,25 +184,112 @@ class GlobalMappingTable(object):
 
         self.conf = confobj
 
-        # bidirection mapping
-        self.l2p = bidict.bidict()
+    def total_entries(self):
+        """
+        total number of entries stored in global mapping table.  It is the same
+        as the number of pages in flash, since we use page-leveling mapping
+        """
+        return self.conf.total_num_pages()
 
-    def has_logical_page(self, logical_page):
-        return logical_page in self.l2p
+    def total_translation_pages(self):
+        """
+        total number of translation pages needed. It is:
+        total_entries * entry size / page size
+        """
+        return (self.total_entries() * \
+            self.conf['dftl']['global_mapping_entry_bytes']) / \
+            self.conf['flash_page_size']
 
-    def has_physical_page(self, physical_page):
-        return physical_page in self.l2p.inv
 
-    def logical_to_physical_page(self, logical_page):
-        return self.l2p[logical_page]
+class GlobalTranslationDirectory(object):
+    def __init__(self, confobj):
+        self.conf = confobj
 
-    def physical_to_logical_page(self, physical_page):
-        return self.l2p[:physical_page]
+        self.flash_npage_per_block = self.conf['flash_npage_per_block']
+        self.flash_num_blocks = self.conf['flash_num_blocks']
+        self.flash_page_size = self.conf['flash_page_size']
+        self.total_pages = self.conf.total_num_pages()
 
-        if flash_page != None:
-            del self.log_page_l2p[:flash_page]
+        self.entry_bytes = self.conf['dftl']['global_mapping_entry_bytes']
+        self.n_entries_per_page = self.flash_page_size / self.entry_bytes
 
-        self.rec.put_and_count("remove_log_page_mapping", lba_page, flash_page)
+        # M_VPN -> M_PPN
+        # Virtual translation page number --> Physical translation page number
+        # Dftl should initialize
+        self.mapping = {}
+
+    def m_vpn_to_m_ppn(self, m_vpn):
+        "m_vpn virtual translation page number"
+        return self.mapping.get(m_vpn, None)
+
+
+class OutOfBandAreas(object):
+    """
+    It is used to hold page state and logical page number of a page.
+    It is not necessary to implement it as list. But the interface should
+    appear to be so.  It consists of page state (bitmap) and logical page
+    number (dict).  Let's proivde more intuitive interfaces: OOB should accept
+    events, and react accordingly to this event. The action may involve state
+    and lpn_of_phy_page.
+    """
+    def __init__(self, confobj):
+        self.flash_num_blocks = confobj['flash_num_blocks']
+        self.flash_npage_per_block = confobj['flash_npage_per_block']
+        self.total_pages = self.flash_num_blocks * flash_npage_per_block
+
+        self.states = ftlbuilder.FlashBitmap2(confobj)
+        self.lpn_of_phy_page = {} #
+
+    def apply_event(self, event):
+        pass
+
+class MappingManager(object):
+    """
+    This class is the supervisor of all the mappings. When initializing, it
+    register CMT and GMT with it and provides higher level operations on top of
+    them.
+    """
+    def __init__(self, cached_mapping_table, global_mapping_table,
+        global_translation_directory, block_pool):
+        self.cached_mapping_table = cached_mapping_table
+        self.global_mapping_table = global_mapping_table
+        self.directory = global_translation_directory
+
+        self.block_pool = block_pool
+
+    def lpn_to_ppn(self, lpn):
+        """
+        This method does not fail. It will try everything to find the ppn of
+        the given lpn.
+        """
+        # try cached mapping table first.
+        ppn = self.cached_mapping_table.lpn_to_ppn(lpn)
+        if ppn == None:
+            # cache miss
+            # TODO:evict one entry from CMT
+
+            # find the physical translation page holding lpn's mapping
+            # Load mapping for lpn from global table
+
+
+        # now the cache should have the ppn, try it again
+        ppn = self.cached_mapping_table.lpn_to_ppn(lpn)
+        assert ppn != None
+        return ppn
+
+    def initialize_mappings(self):
+        """
+        This function initialize all global mapping table and global mapping
+        directory. We assume the GTD is very small and stored in flash before
+        mounting. We also assume that the global mapping table has been prepared
+        by the vendor, so there is no other overhead except for reading the GTD
+        from flash. Since the overhead is very small, we ignore it.
+        """
+        total_pages = self.global_mapping_table.total_translation_pages()
+        # use the pages in the beginning of the flash as translation pages
+        for m_vpn in range(total_pages):
+            self.directory.add_mapping(m_vpn=m_vpn, m_ppn=m_vpn)
+
 
 
 class Dftl(ftlbuilder.FtlBuilder):
@@ -187,24 +300,58 @@ class Dftl(ftlbuilder.FtlBuilder):
         super(HybridMapFtl, self).__init__(confobj, recorderobj, flashobj)
 
         # bitmap has been created parent class
-        self.bitmap.initialize()
+        # Change: we now don't put the bitmap here
+        # self.bitmap.initialize()
+        del self.bitmap
 
         # initialize free list
         self.block_pool = BlockPool(self.conf['flash_num_blocks'])
 
+        self.global_mapping_table = GlobalMappingTable(confobj, flashobj)
+        self.cached_mapping_table = CachedMappingTable(confobj)
+        self.global_translation_directory = GlobalTranslationDirectory(confobj)
+
+        # register the mapping data structures with the manger
+        # later you may also need to register them with the cleaner
+        self.mapping_manager = MappingManager(
+            cached_mapping_table = self.cached_mapping_table,
+            global_mapping_table = self.global_mapping_table,
+            global_translation_directory = self.global_translation_directory,
+            block_pool = self.block_pool)
+
+        # TODO: we should initialize Globaltranslationdirectory in Dftl
+        # By default, let's use the first n pages of the translation
+
+        self.oob = OutOfBandAreas(confobj)
+
     # FTL APIs
-    def lba_read(self, pagenum):
+    def lba_read(self, lpn):
+        """
+        ppn = translate(pagenum))
+        flash.read(ppn)
+        """
+        ppn = self.mapping_manager.lpn_to_ppn(lpn)
+        self.flash.page_read(ppn, 'user')
+
+    def lba_write(self, lpn):
+        """
+        """
         pass
 
-    def lba_write(self, pagenum):
+    def lba_discard(self, lpn):
+        """
+        oob.apply_event(event)
+        This should invalidate the page and remove the LPN in OOB
+        """
         pass
 
-    def lba_discard(self, pagenum):
+    def lpn_to_ppn(self, lpn):
         pass
 
-    #
 
+def main():
+    pass
 
-
-
+if __name__ == '__main__':
+    main()
 
