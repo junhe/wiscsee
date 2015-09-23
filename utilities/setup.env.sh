f78311ab49935d6c125202bd89e2e1bdc16c1d25
jhe <jhe@node-0.fordora.fsperfatscale-pg0.wisc.cloudlab.us>
2015-09-23 13:06:34 -0500

    Add cmake mpich chopper

--- utilities/setup.env.sh
+++ utilities/setup.env.sh
@@ -1,1 +1,10 @@
-sudo apt-get install btrfs-tools f2fs-tools
+#sudo apt-get update
+#sudo apt-get install btrfs-tools f2fs-tools
+
+#sudo apt-get install cmake
+
+#sudo apt-get install mpich mpich-doc libmpich-dev
+
+cd ../../chopper
+cmake CMakeLists.txt
+make
