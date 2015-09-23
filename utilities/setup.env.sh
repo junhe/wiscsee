beea1fe77e757ddf891ca817a776f6cf4a06abcc
jhe <jhe@node-0.fordora.fsperfatscale-pg0.wisc.cloudlab.us>
2015-09-23 13:34:11 -0500

    Add more necessary dependencies

--- utilities/setup.env.sh
+++ utilities/setup.env.sh
@@ -1,10 +1,23 @@
-#sudo apt-get update
-#sudo apt-get install btrfs-tools f2fs-tools
+# This file should be executed within the current dir
+doradir=`pwd`
+sudo apt-get update
+sudo apt-get install -y btrfs-tools f2fs-tools
 
-#sudo apt-get install cmake
+sudo apt-get install -y cmake
 
-#sudo apt-get install mpich mpich-doc libmpich-dev
+sudo apt-get install -y mpich mpich-doc libmpich-dev
 
-cd ../../chopper
+cd ../../
+git clone https://github.com/junhe/chopper.git
+cd chopper
 cmake CMakeLists.txt
 make
+cd $doradir
+
+sudo apt-get install -y python-bitarray
+
+sudo apt-get install -y python-pip
+sudo pip install bidict
+
+sudo apt-get install -y blktrace
+
