4d238472c64bb6a905ccdf45a30ec68a78276cb6
Jun He <jhe@cs.wisc.edu>
2016-02-15 09:30:09 -0600

    Add code for integrated test

--- dftlncq_test.py
+++ dftlncq_test.py
@@ -199,8 +199,53 @@ class TestFTLwithDFTLFLEX2(unittest.TestCase):
         self.setup_ftl()
         self.my_run()
 
+class TestFTLwithDFTLIntegrated(unittest.TestCase):
+    def setup_config(self):
+        self.conf = config.ConfigNCQFTL()
+        self.conf['dftlncq']['ncq_depth'] = 2
 
+        self.conf['flash_config']['n_pages_per_block'] = 128
+        self.conf['flash_config']['n_blocks_per_plane'] = 32
+        self.conf['flash_config']['n_planes_per_chip'] = 1
+        self.conf['flash_config']['n_chips_per_package'] = 1
+        self.conf['flash_config']['n_packages_per_channel'] = 1
+        self.conf['flash_config']['n_channels_per_dev'] = 8
 
+    def setup_environment(self):
+        metadata_dic = choose_exp_metadata(self.conf, interactive = False)
+        self.conf.update(metadata_dic)
+
+        self.conf['enable_blktrace'] = True
+        self.conf['enable_simulation'] = True
+
+    def setup_workload(self):
+        self.conf["workload_src"] = LBAGENERATOR
+        self.conf["lba_workload_class"] = "ExtentTestWorkload"
+        self.conf["lba_workload_configs"]["ExtentTestWorkload"] = {
+            "op_count": 1000}
+        self.conf["age_workload_class"] = "NoOp"
+
+    def setup_ftl(self):
+        self.conf['ftl_type'] = 'ftlwdftl'
+        self.conf['simulator_class'] = 'SimulatorDES'
+
+        devsize_mb = 1024
+        entries_need = int(devsize_mb * 2**20 * 0.03 / self.conf['flash_config']['page_size'])
+        self.conf['dftl']['max_cmt_bytes'] = int(entries_need * 8) # 8 bytes (64bits) needed in mem
+        self.conf.set_flash_num_blocks_by_bytes(int(devsize_mb * 2**20 * 1.28))
+        print "Current n_blocks_per_plane",\
+            self.conf['flash_config']['n_blocks_per_plane']
+
+    def my_run(self):
+        runtime_update(self.conf)
+        workflow(self.conf)
+
+    def _test_main(self):
+        self.setup_config()
+        self.setup_environment()
+        self.setup_workload()
+        self.setup_ftl()
+        self.my_run()
 
 
 def main():
