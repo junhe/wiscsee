fbcad48a8e54ed78373b2453f18bf1329e29ca62
Jun He <jhe@cs.wisc.edu>
2016-02-29 08:03:37 -0600

    Refactor file system configuration

--- WlRunner/wlrunner.py
+++ WlRunner/wlrunner.py
@@ -65,29 +65,6 @@ class WorkloadRunner(object):
                 format(type(confobj).__name__()))
         self.conf = confobj
 
-        if self.conf.device_type == 'loop':
-            # create loop dev object, it is not physically created yet
-            self.loopdev = filesystem.LoopDevice(
-                dev_path = self.conf['device_path'],
-                tmpfs_mount_point = self.conf['tmpfs_mount_point'],
-                size_mb = self.conf['dev_size_mb'])
-
-        # create file system object, it is not physically created
-        fs = self.conf['filesystem']
-        if fs == 'ext4':
-            fsclass = filesystem.Ext4
-        elif fs == 'f2fs':
-            fsclass = filesystem.F2fs
-        elif fs == 'btrfs':
-            fsclass = filesystem.Btrfs
-        elif fs == 'xfs':
-            fsclass = filesystem.Xfs
-        else:
-            raise RuntimeError("{} is not a valid file system type"\
-                .format(fs))
-        self.fs = fsclass(device = self.conf['device_path'],
-            mount_point = self.conf['fs_mount_point'])
-
         # blktracer for making file system
         self.blktracer_mkfs = blocktrace.BlockTraceManager(
             dev = self.conf['device_path'],
@@ -116,16 +93,17 @@ class WorkloadRunner(object):
                 wlclass = self.conf["workload_class"],
                 wlconf_key = self.conf["workload_conf_key"]
                 ) #format
-        print workload_str
         self.workload = eval(workload_str)
 
-        # create Ftrace manager
-        # self.ftrace = ftrace.Ftrace()
+    def prepare_device(self):
 
-    def prepare_fs(self):
-        # Prepare file systems
         if self.conf.device_type == 'loop':
+            self.loopdev = filesystem.LoopDevice(
+                dev_path = self.conf['device_path'],
+                tmpfs_mount_point = self.conf['tmpfs_mount_point'],
+                size_mb = self.conf['dev_size_mb'])
             self.loopdev.create()
+
         elif self.conf.device_type == 'real':
             # umount file system if it is mounted
             if fshelper.isMounted(self.conf['fs_mount_point']):
@@ -149,13 +127,32 @@ class WorkloadRunner(object):
             fshelper.partition_disk(base_dev_path, part_sizes)
 
     def build_fs(self):
+        # create file system object, it is not physically created
+        fs = self.conf['filesystem']
+        if fs == None:
+            return
+
+        if fs == 'ext4':
+            fsclass = filesystem.Ext4
+        elif fs == 'f2fs':
+            fsclass = filesystem.F2fs
+        elif fs == 'btrfs':
+            fsclass = filesystem.Btrfs
+        elif fs == 'xfs':
+            fsclass = filesystem.Xfs
+        else:
+            raise RuntimeError("{} is not a valid file system type"\
+                .format(fs))
+        fs_obj = fsclass(device = self.conf['device_path'],
+            mount_point = self.conf['fs_mount_point'])
+
         # Making and mounting file system
         try:
             mk_opt_dic = self.conf[self.conf['filesystem']].get('make_opts', None)
         except KeyError:
             mk_opt_dic = None
-        self.fs.make(opt_dic = mk_opt_dic)
-        self.fs.mount(opt_list =
+        fs_obj.make(opt_dic = mk_opt_dic)
+        fs_obj.mount(opt_list =
             self.conf['mnt_opts'][ self.conf['filesystem'] ])
         utils.shcmd('sync')
 
@@ -163,7 +160,7 @@ class WorkloadRunner(object):
         if self.conf['filesystem'] == 'f2fs':
             for opt_name, value in \
                 self.conf['f2fs'].get('sysfs', {}).items():
-                self.fs.sysfs_setup(opt_name, value)
+                fs_obj.sysfs_setup(opt_name, value)
 
     def run(self):
         if self.conf['enable_blktrace'] == True:
@@ -174,7 +171,7 @@ class WorkloadRunner(object):
     def run_without_blktrace(self):
         cpuhandler.set_cpus(self.conf['n_online_cpus'])
 
-        self.prepare_fs()
+        self.prepare_device()
         self.build_fs()
 
         self.aging_workload.run()
@@ -188,7 +185,7 @@ class WorkloadRunner(object):
             # Set number of CPUs
             cpuhandler.set_cpus(self.conf['n_online_cpus'])
 
-            self.prepare_fs()
+            self.prepare_device()
 
             # strat blktrace
             # This is only for making and mounting file system, because we
