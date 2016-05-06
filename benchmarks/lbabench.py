3ba3309ea727baf9ba483c16aeba16695d07cd0e
Jun He <jhe@cs.wisc.edu>
2016-05-06 08:25:06 -0500

    Config exp

--- benchmarks/lbabench.py
+++ benchmarks/lbabench.py
@@ -303,17 +303,17 @@ def patterns_bench():
         expname = get_expname()
         para_dict = {
                 'expname'        : [expname],
-                'ncq_depth'      : [4],
-                # 'patternclass'   : ['SRandomRead'],
+                'ncq_depth'      : [4, 8],
+                'patternclass'   : ['SRandomRead'],
                 # 'patternclass'   : ['SRandomRead', 'SSnake'],
-                'patternclass'   : ['SHotNCold',
-                    'SRandomWrite', 'SRandomRead',
-                    'SSequentialWrite', 'SSequentialRead',
-                    'SSnake', 'SFadingSnake', 'SStrided'],
+                # 'patternclass'   : ['SHotNCold',
+                    # 'SRandomWrite', 'SRandomRead',
+                    # 'SSequentialWrite', 'SSequentialRead',
+                    # 'SSnake', 'SFadingSnake', 'SStrided'],
                 'cache_mapped_data_bytes' :[8*MB, 128*MB],
                 'flashbytes'     : [128*MB],
-                'stripe_size'    : [1, 'infinity'],
-                'chunk_size'     : [2*KB, 128*KB]
+                'stripe_size'    : [1],
+                'chunk_size'     : [2*KB]
                 }
 
         parameter_combs = ParameterCombinations(para_dict)
@@ -322,9 +322,8 @@ def patterns_bench():
 
     cnt = 0
     for i, para in enumerate(gen_parameters()):
+        print 'exp', cnt
         cnt += 1
-        if cnt != 10:
-            continue
         Parameters = collections.namedtuple("Parameters", ','.join(para.keys()))
         exp = Experiment( Parameters(**para) )
         exp.main()
