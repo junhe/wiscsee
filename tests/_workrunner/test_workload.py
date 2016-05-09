76e8fb1cc04e7db876f49d16b8606c3df4442a14
Jun He <jhe@cs.wisc.edu>
2016-05-09 14:32:49 -0500

    Add test file

---
+++ tests/_workrunner/test_workload.py
@@ -0,0 +1,34 @@
+import unittest
+import os
+
+from commons import *
+import ssdbox
+from workrunner.workload import PatternSuite
+
+class TestPatternSuite(unittest.TestCase):
+    def test_random(self):
+        conf = ssdbox.dftldes.Config()
+
+        # for test
+        conf['fs_mount_point'] = '/tmp/'
+
+        conf['workload_class'] = 'PatternSuite'
+        conf['workload_conf_key'] = 'PatternSuite'
+        conf['PatternSuite'] = {'patternname': 'SRandomWrite',
+                'parameters': {
+                    'zone_size': 1*MB,
+                    'chunk_size': 512*KB,
+                    'traffic_size': 1*MB,
+                    }
+                }
+
+        wl = PatternSuite(conf, workload_conf_key='PatternSuite')
+        wl.run()
+
+        self.assertEqual(os.path.exists('/tmp/datafile'), True)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
