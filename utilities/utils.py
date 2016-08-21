3de3ab2d3174f529ea4aadc3f1aa32002481367b
Jun He <jhe@cs.wisc.edu>
2016-08-20 22:04:43 -0500

    Add get_dir_size

--- utilities/utils.py
+++ utilities/utils.py
@@ -302,3 +302,12 @@ def replicate_parameters(parameter_combs, n):
     return result
 
 
+
+def get_dir_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
