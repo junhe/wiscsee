dd719ad4dec01fd6f3d7edc77b01dba006ab984d
Jun He <jhe@cs.wisc.edu>
2016-09-01 20:53:24 -0500

    Config varmail cache ratio

--- benchmarks/appbench.py
+++ benchmarks/appbench.py
@@ -895,7 +895,7 @@ def varmailbench_for_locality():
                     'ftl'            : ['dftldes'],
                     'filesystem'     : ['f2fs', 'xfs', 'ext4', 'btrfs'],
                     'cache_mapped_data_bytes' :[
-                        int(0.1, * lbabytes),
+                        int(0.1 * lbabytes),
                         int(0.5 * lbabytes),
                         int(1 * lbabytes)
                         # int(0.05 * lbabytes),
