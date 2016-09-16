18e9594604621b60e0aa5b60149569dc4a66fb33
Jun He <jhe@cs.wisc.edu>
2016-09-16 10:12:53 -0500

    Add ext4 lpn classifier

--- ssdbox/ftlcounter.py
+++ ssdbox/ftlcounter.py
@@ -18,6 +18,12 @@ from utilities import utils
 from .blkpool import BlockPool
 from .bitmap import FlashBitmap2
 
+import prepare4pyreuse
+from pyreuse.sysutils import blocktrace, blockclassifiers, dumpe2fsparser
+from pyreuse.fsutils import ext4dumpextents
+
+
+
 class Config(config.ConfigNCQFTL):
     def __init__(self, confdic = None):
         super(Config, self).__init__(confdic)
@@ -139,14 +145,15 @@ class Ftl(ftlbuilder.FtlBuilder):
         # print self.read_count
         # print self.write_count
         # print self.discard_count
-        self.dump_counts()
+        self.do_stats()
 
-    def dump_counts(self):
-        lpns = list(set(self.read_count.keys()
-            + self.write_count.keys()
-            + self.discard_count.keys()))
-        lpns = sorted(lpns)
+    def do_stats(self):
+        lpns = self.get_lpns()
 
+        self.dump_counts(lpns)
+        self.dump_lpn_sem(lpns)
+
+    def dump_counts(self, lpns):
         counters = [self.read_count, self.write_count, self.discard_count]
 
         table = []
@@ -164,11 +171,101 @@ class Ftl(ftlbuilder.FtlBuilder):
                 f.write(row)
                 f.write('\n')
 
+        self.clean_up()
+
+    def dump_lpn_sem(self, lpns):
+        classifier = LpnClassification(
+                lpns = lpns,
+                device_path = self.conf['device_path'],
+                result_dir = self.conf['result_dir'],
+                flash_page_size = 2048)
+        table = classifier.classify()
+
+        lpn_sem_path = os.path.join(self.conf['result_dir'], 'lpn_sem.out')
+        with open(lpn_sem_path, 'w') as f:
+            f.write(utils.table_to_str(table, width=0))
+
+
+    def get_lpns(self):
+        lpns = list(set(self.read_count.keys()
+            + self.write_count.keys()
+            + self.discard_count.keys()))
+        lpns = sorted(lpns)
+
+        return lpns
+
+    def clean_up(self):
         with utils.cd(self.conf['result_dir']):
             utils.shcmd('rm blkparse*', ignore_error=True)
 
-
     def get_type(self):
         return "ftlcounter"
+
+
+class LpnClassification(object):
+    def __init__(self, lpns, device_path, result_dir, flash_page_size):
+        self.device_path = device_path
+        self.result_dir = result_dir
+        self.flash_page_size = flash_page_size
+        self.lpns = lpns
+
+        self.dumpe2fs_out_path = os.path.join(self.result_dir, 'dumpe2fs.out')
+        self.extents_path = os.path.join(self.result_dir, 'extents.json')
+        self.fs_block_size = 4096
+
+    def classify(self):
+        extents = self._get_extents()
+        filepath_classifier = blockclassifiers.Ext4FileClassifier(extents,
+                self.fs_block_size)
+
+        range_table = self._get_range_table()
+        classifier = blockclassifiers.Ext4BlockClassifier(range_table,
+                self.fs_block_size)
+
+        table = []
+        for lpn in self.lpns:
+            offset  = lpn * self.flash_page_size
+            sem = classifier.classify(offset)
+            if sem == 'UNKNOWN':
+                sem = filepath_classifier.classify(offset)
+            row = {'lpn': lpn,
+                   'sem':sem}
+            table.append(row)
+
+        return table
+
+    def _get_extents(self):
+        d = utils.load_json(self.extents_path)
+        extents = d['extents']
+
+        return extents
+
+    def _get_range_table(self):
+        with open(self.dumpe2fs_out_path, 'r') as f:
+            text = f.read()
+
+        header_text, bg_text = text.split("\n\n\n")
+
+        range_table = dumpe2fsparser.parse_bg_text(bg_text)
+
+        j_start, j_end = self._get_journal_block_ext(header_text)
+        if j_start != -1:
+            range_table.append( {'journal': (j_start, j_end)} )
+
+        return range_table
+
+    def _get_journal_block_ext(self, header_text):
+        header_dict = dumpe2fsparser.parse_header_text(header_text)
+
+        if header_dict.has_key('journal-inode') is not True:
+            return -1, -1
+
+        journal_inum = header_dict['journal-inode']
+        journal_len = header_dict['journal-length']
+
+        ext_text = ext4dumpextents.dump_extents_of_a_file(self.device_path,
+                '<{}>'.format(journal_inum))
+        table = ext4dumpextents.parse_dump_extents_output(ext_text)
+        return table[0]['Physical_start'], table[0]['Physical_end']
 
 
