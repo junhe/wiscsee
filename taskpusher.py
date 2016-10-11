1cf462453e57bb9536ed64ecc2e6cfe8e495e57d
Jun He <jhe@cs.wisc.edu>
2016-10-11 13:12:41 -0500

    Add taskpusher and varmail locality

---
+++ taskpusher.py
@@ -0,0 +1,15 @@
+from celerytasks import app, exec_sim, add
+import time
+
+from utilities.utils import load_json
+
+d = load_json('./varmail-localoty.json')
+para = d['para_dicts'][0]
+para['expname'] = 'testxxx'
+
+# exec_sim.delay(para)
+print app.conf.BROKER_URL
+app.conf.BROKER_URL = 'amqp://guest@node-2//'
+
+add.delay(4, 88)
+
