8f7c09f4465c2a1369cbae839f84e617d84319e2
Jun He <jhe@cs.wisc.edu>
2016-10-11 12:28:49 -0500

    Integrate execute simulation to celerytasks

--- celerytasks.py
+++ celerytasks.py
@@ -1,8 +1,9 @@
 from celery import Celery
+from benchmarks.appbench import execute_simulation
 
 app = Celery('tasks', broker='amqp://guest@localhost//')
 
 @app.task
-def pass_dict(para_dict):
-        print para_dict
-        return para_dict
+def exec_sim(para_dict):
+    execute_simulation(para_dict)
+
